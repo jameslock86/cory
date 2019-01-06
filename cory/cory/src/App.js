deef232d9e68ddf79ed44d0315dd583dd5ea66fa
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2019-01-06 02:59:34 -0600

    adding some styling to the top of the page

--- cory/cory/src/App.js
+++ cory/cory/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/home.js';
 import About from './components/about.js';
 import Topics from './components/topics.js';
+import Music from './components/music.js';
 import CoryImage from './public/coryImage.jpg';
 class App extends Component {
   render() {
@@ -11,16 +12,18 @@ class App extends Component {
       <HashRouter >
        <div>
          <img id="hero-image"src={CoryImage} alt="hero Image"/>
-          <h1 id="hero-title">PunkBordello</h1>
+          <h1 id="hero-title">Punk Bordello</h1>
           <ul className="header">
-            <a className="navLink"><NavLink to="/">Home</NavLink></a>
-            <a className="navLink"><NavLink to="/about">About</NavLink></a>
-           <a className="navLink"><NavLink to="/topics">Topics</NavLink></a>
+            <a><NavLink className="navLink" to="/">Home</NavLink></a>
+            <a><NavLink className="navLink" to="/about">About</NavLink></a>
+           <a><NavLink className="navLink" to="/topics">Topics</NavLink></a>
+            <a><NavLink className="navLink" to="/music">Music</NavLink></a>
           </ul>
           
             <Route path="/"exact component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/topics" component={Topics}/>
+            <Route path="/music" component={Music}/>
           </div>
         
       </HashRouter >
---
+++ cory/cory/src/components/music.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Music extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Music </h2>
+        <p>Soundcloud, bandcamp, Spotify, Apple Music, I paid for
+           pretty much everything to be available 
+          everywhere and as long as you search PunkBordello
+           it’ll always be the first thing to come up</p>
+ 
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Music;
